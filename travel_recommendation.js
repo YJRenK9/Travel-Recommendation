799181e762aad1f1f9d822355f416a04bdde8261
YJ <yjrenk9@github.com>
2026-03-31 04:25:42 -0400

    implemented all the HTML files

---
+++ travel_recommendation.js
@@ -0,0 +1,8 @@
+fetch("travel_recommendation_api.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => {
+        console.log(error);
+    });

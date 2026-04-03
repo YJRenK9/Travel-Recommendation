3cf052dea957035d489a7adace3fff20bada8fb3
YJRenK9 <42582477+YJRenK9@users.noreply.github.com>
2026-04-03 13:42:19 -1000

    Added bonus task
    implemented a feature to display the selected time zones via a dropdown

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -5,6 +5,31 @@ var searchResults = document.getElementById("search-results");
 
 var homeDiv = document.getElementById("home");
 
+var timezoneSelect = document.getElementById("time-zone-select");
+var timezoneInfo = document.getElementById("time-zone-info");
+var currentTime = document.getElementById("current-time");
+
+var timezonesMap = new Map();
+timezonesMap.set("LT", "Local Time");
+timezonesMap.set("UTC", "Coordinated Universal Time");
+timezonesMap.set("Pacific/Honolulu", "Hawaii Standard Time");
+timezonesMap.set("America/Phoenix", "Mountain Standard Time");
+timezonesMap.set("America/Cancun", "Eastern Standard Time");
+timezonesMap.set("America/Sao_Paulo", "Brasília Time");
+timezonesMap.set("Asia/Kolkata", "India Standard Time");
+timezonesMap.set("Asia/Bangkok", "Indochina Time");
+timezonesMap.set("Asia/Tokyo", "Japan Standard Time");
+timezonesMap.set("Australia/Sydney", "Australian Eastern Standard Time");
+
+var currentTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+console.log(currentTimezone);
+var selectedTime = new Date().toLocaleTimeString("en-US", { timeZone: currentTimezone });
+
+
+timezoneInfo.innerHTML = `${timezonesMap.get(timezoneSelect.value)}`;
+console.log(timezoneSelect.value);
+currentTime.innerHTML = `Current Time: ${selectedTime}`;
+
 fetch("travel_recommendation_api.json")
     .then(response => response.json())
     .then(data => {
@@ -107,6 +132,14 @@ fetch("travel_recommendation_api.json")
 
             //show the home div when the search results are cleared
             homeDiv.style.display = "block";
+        });
+
+        // when the user selects a different timezone from the dropdown, update the selected time and the timezone information displayed on the webpage
+        timezoneSelect.addEventListener("change", () => {
+            var selectedTimezone = (timezoneSelect.value !== "LT") ? timezoneSelect.value : undefined;
+            var selectedTime = new Date().toLocaleTimeString("en-US", { timeZone: selectedTimezone });
+            currentTime.innerHTML = `Current Time: ${selectedTime}`;
+            timezoneInfo.innerHTML = `${(!selectedTimezone) ? "Local Time" : timezonesMap.get(selectedTimezone)}`;
         });
     })
     .catch(error => {
